4585d2abd80550e5d50808fb46b2fd5063e74f2f
wildwhale <wild.whale.jin@gmail.com>
2018-11-15 22:23:42 +0900

    Whale h/w templete

---
+++ Chapter01-good-old-times/wild-whale/1.1/src/main/java/com/whale/Exercises/in/Programming/Style/chapter1/GoodOldTimes.java
@@ -0,0 +1,7 @@
+package com.whale.Exercises.in.Programming.Style.chapter1;
+
+public class GoodOldTimes {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
