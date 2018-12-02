6b6313ba1f8165ee7c5b23bb5369d437dbe0a423
wildwhale <wild.whale.jin@gmail.com>
2018-12-02 15:50:36 +0900

    Chapter2 go-forth source upload

--- Chapter01-good-old-times/wild-whale/1.1/src/main/java/com/whale/excercises4/IndexManager.java
+++
@@ -1,9 +0,0 @@
-package com.whale.excercises.in.programming.style.chapter1.v3;
-
-import java.io.IOException;
-
-public interface IndexManager {
-    void record(String term, int pageNum) throws IOException;
-    void print() throws IOException;
-    void close() throws IOException;
-}
